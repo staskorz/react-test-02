714e067f24d5d752d9f685b905afad79a8e73814
staskorz <staskorz@gmail.com>
2018-07-01 21:27:33 +0300

    Integrated ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+
+  plugins: ["prettier", "react"],
+
+  // add your custom rules here
+  rules: {
+    // allow debugger during development
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+}
