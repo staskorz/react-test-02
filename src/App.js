fa6c0142317c411e5a8ae43ec7774368b2739a30
staskorz <staskorz@gmail.com>
2018-07-03 23:16:06 +0300

    Design attempt 1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
 import React from "react"
 
-export default () => <div>Welcome to React</div>
+export default () => (
+  <div className="books-container">
+    <div className="book">
+      <p>Title</p>
+    </div>
+    <div className="book" />
+    <div className="book" />
+    <div className="book" />
+  </div>
+)
