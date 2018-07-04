46813a4490283c4aa01b55b85a4f59ca3e75b4a1
staskorz <staskorz@gmail.com>
2018-07-04 22:08:59 +0300

    Added title

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react"
 
 export default () => (
-  <div className="books-container">
-    <div className="book">
-      <div className="author">Author</div>
-      <div className="title">Title Title Title Title</div>
-      <div className="publication-date">1948</div>
+  <div>
+    <h1>Books</h1>
+    <div className="books-container">
+      <div className="book">
+        <div className="author">Author</div>
+        <div className="title">Title Title Title Title</div>
+        <div className="publication-date">1948</div>
+      </div>
+      <div className="book" />
+      <div className="book" />
+      <div className="book" />
     </div>
-    <div className="book" />
-    <div className="book" />
-    <div className="book" />
   </div>
 )
