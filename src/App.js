41cc1bb893c90284fa1803f64e72ba134749404f
staskorz <staskorz@gmail.com>
2018-07-04 21:53:04 +0300

    Migrated to Google Fonts for "ancient" font style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react"
 export default () => (
   <div className="books-container">
     <div className="book">
-      <p>Title</p>
+      <div className="author">Author</div>
+      <div className="title">Title Title Title Title</div>
+      <div className="publication-date">1948</div>
     </div>
     <div className="book" />
     <div className="book" />
