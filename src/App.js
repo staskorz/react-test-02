c73bb374dbb3caf1de46d592ef581827af127f9f
staskorz <staskorz@gmail.com>
2018-07-05 17:38:46 +0300

    Enabled fetching fake books from server

--- src/App.js
+++ src/App.js
@@ -2,55 +2,10 @@ import React from "react"
 
 import BookList from "./components/BookList"
 
-const books = [
-  {
-    id: 123,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-  {
-    id: 124,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-  {
-    id: 125,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-  {
-    id: 126,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-  {
-    id: 127,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-  {
-    id: 128,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-  {
-    id: 129,
-    author: "Author Author",
-    title: "Title Title Title Title",
-    publicationDate: "1948",
-  },
-]
-
 export default () => (
   <div>
     <h1>Books</h1>
 
-    <BookList books={books} />
+    <BookList />
   </div>
 )
--- src/components/BookList/index.js
+++ src/components/BookList/index.js
@@ -1,7 +1,33 @@
-import { compose } from "recompose"
+import { compose, withProps, lifecycle } from "recompose"
+
+import mountYearFromDate from "../../util/month-year-from-date"
 
 import BookList from "./BookList"
 
-const enhance = compose()
+const enhance = compose(
+  withProps({
+    books: [],
+  }),
+  lifecycle({
+    componentDidMount() {
+      console.log("in componentDidMount()")
+      // const { setState } = this
+
+      fetch("/fake-books.json")
+        .then(response => {
+          console.log(response)
+          return response.json()
+        })
+        .then(books =>
+          books.map(({ authorName, publicationDate, ...rest }) => ({
+            author: authorName,
+            publicationDate: mountYearFromDate(publicationDate),
+            ...rest,
+          })),
+        )
+        .then(books => this.setState({ books }))
+    },
+  }),
+)
 
 export default enhance(BookList)
