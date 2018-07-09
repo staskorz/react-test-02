6bff5d2fdd2f9498a3f83e4094bc4c4d5da7b3c6
staskorz <staskorz@gmail.com>
2018-07-09 15:05:21 +0300

    Finished Book Edit form design

--- src/components/EditBookModal.js
+++ src/components/EditBookModal.js
@@ -4,15 +4,65 @@ import PropTypes from "prop-types"
 import Modal from "./Modal"
 
 const Component = ({
-  book: { id, title, author, publicationDate },
+  book: {
+    id,
+    title,
+    author,
+    publicationDate,
+    titleValidationError,
+    authorValidationError,
+    publicationDateValidationError,
+  },
   onClose,
 }) => (
   <Modal onClose={onClose}>
-    <div>
-      <div>
-        <label htmlFor="title">Title</label>
+    <div className="edit-book-form">
+      <div className="form-title">Edit book</div>
+
+      <div className="form-controls">
+        <div>
+          <div>
+            <label htmlFor="author">Author</label>
+          </div>
+
+          <div>
+            <input type="text" id="author" value={author} />
+          </div>
+
+          <div className="validation-error">{authorValidationError}</div>
+        </div>
+
+        <div>
+          <div>
+            <label htmlFor="title">Title</label>
+          </div>
+
+          <div>
+            <input type="text" id="title" value={title} />
+          </div>
+
+          <div className="validation-error">{titleValidationError}</div>
+        </div>
+
+        <div>
+          <div>
+            <label htmlFor="date">Date</label>
+          </div>
+
+          <div>
+            <input type="text" id="date" value={publicationDate} />
+          </div>
+
+          <div className="validation-error">
+            {publicationDateValidationError}
+          </div>
+        </div>
       </div>
-      <input type="text" id="title" />
+
+      <div>
+        <button>Submit</button>
+        <button onClick={onClose}>Cancel</button>
+      </div>
     </div>
   </Modal>
 )
