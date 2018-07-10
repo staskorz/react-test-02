560ba0391fb33c5bbb4a09681b652842d70c842c
staskorz <staskorz@gmail.com>
2018-07-11 01:46:10 +0300

    Made path to spinner image relative

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default () => (
   <div className="spinner-container">
-    <img src="/spinner.gif" alt="spinner" width="64px" height="64px" />
+    <img src="spinner.gif" alt="spinner" width="64px" height="64px" />
   </div>
 )
