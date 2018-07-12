c10e99cbdb095aa6218bf091a41aae9a86e258ff
staskorz <staskorz@gmail.com>
2018-07-12 13:55:55 +0300

    Changed book ID generation from array length to current milliseconds

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -37,7 +37,7 @@ const Component = ({
       <EditBookModal
         onClose={onAddModalClose}
         book={{
-          id: books.length,
+          id: Date.now(),
           title: "",
           author: "",
           publicationDate: new Date(),
